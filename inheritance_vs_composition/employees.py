4afb5ab58db0d0cd91b6db3f29f0d0add5912181
Anh Tu <sweantu@gmail.com>
2025-02-15 14:56:39 +0700

    feat: inheritance vs composite

--- inheritance_vs_composition/employees.py
+++ inheritance_vs_composition/employees.py
@@ -1,39 +1,89 @@
 from abc import ABC, abstractmethod
-from hr import SalaryPolicy, HourlyPolicy, CommissionPolicy
-from productivity import ManagerRole, SecretaryRole, SalesPersonRole, FactoryWorkerRole
+from hr import PayrollSystem, SalaryPolicy, HourlyPolicy, CommissionPolicy
+from contacts import AddressBook
+from productivity import (
+    ManagerRole,
+    ProductivitySystem,
+    SecretaryRole,
+    SalesPersonRole,
+    FactoryWorkerRole,
+)
+
+# class Employee(ABC):
+#     def __init__(self, id, name):
+#         self.id = id
+#         self.name = name
+#         self.address = None
 
 
-class Employee(ABC):
-    def __init__(self, id, name):
+# class Manager(Employee, ManagerRole, SalaryPolicy):
+#     def __init__(self, id, name, weekly_salary):
+#         SalaryPolicy.__init__(self, weekly_salary)
+#         Employee.__init__(self, id, name)
+
+
+# class Secretary(Employee, SecretaryRole, SalaryPolicy):
+#     def __init__(self, id, name, weekly_salary):
+#         SalaryPolicy.__init__(self, weekly_salary)
+#         Employee.__init__(self, id, name)
+
+
+# class SalesPerson(Employee, SalesPersonRole, CommissionPolicy):
+#     def __init__(self, id, name, weekly_salary, commission):
+#         CommissionPolicy.__init__(self, weekly_salary, commission)
+#         Employee.__init__(self, id, name)
+
+
+# class FactoryWorker(Employee, FactoryWorkerRole, HourlyPolicy):
+#     def __init__(self, id, name, hours_worked, hourly_rate):
+#         HourlyPolicy.__init__(self, hours_worked, hourly_rate)
+#         Employee.__init__(self, id, name)
+
+
+# class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
+#     def __init__(self, id, name, hours_worked, hourly_rate):
+#         HourlyPolicy.__init__(self, hours_worked, hourly_rate)
+#         Employee.__init__(self, id, name)
+
+
+class EmployeeDatabase:
+    def __init__(self):
+        self._employees = [
+            {"id": 1, "name": "Mary Poppins", "role": "manager"},
+            {"id": 2, "name": "John Smith", "role": "secretary"},
+            {"id": 3, "name": "Kevin Bacon", "role": "sales"},
+            {"id": 4, "name": "Jane Doe", "role": "factory"},
+            {"id": 5, "name": "Robin Williams", "role": "secretary"},
+        ]
+        self.productivity = ProductivitySystem()
+        self.payroll = PayrollSystem()
+        self.employee_addresses = AddressBook()
+
+    @property
+    def employees(self):
+        return [self._create_employee(**data) for data in self._employees]
+
+    def _create_employee(self, id, name, role):
+        address = self.employee_addresses.get_employee_address(id)
+        employee_role = self.productivity.get_role(role)
+        payroll_policy = self.payroll.get_policy(id)
+        return Employee(id, name, address, employee_role, payroll_policy)
+
+
+class Employee:
+    def __init__(self, id, name, address, role, payroll):
         self.id = id
         self.name = name
+        self.address = address
+        self.role = role
+        self.payroll = payroll
 
+    def work(self, hours):
+        duties = self.role.work(hours)
+        print(f"Employee {self.id} - {self.name}:")
+        print(f"- {duties}")
+        print("")
+        self.payroll.track_work(hours)
 
-class Manager(Employee, ManagerRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        Employee.__init__(self, id, name)
-
-
-class Secretary(Employee, SecretaryRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        Employee.__init__(self, id, name)
-
-
-class SalesPerson(Employee, SalesPersonRole, CommissionPolicy):
-    def __init__(self, id, name, weekly_salary, commission):
-        CommissionPolicy.__init__(self, weekly_salary, commission)
-        Employee.__init__(self, id, name)
-
-
-class FactoryWorker(Employee, FactoryWorkerRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hourly_rate):
-        HourlyPolicy.__init__(self, hours_worked, hourly_rate)
-        Employee.__init__(self, id, name)
-
-
-class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hourly_rate):
-        HourlyPolicy.__init__(self, hours_worked, hourly_rate)
-        Employee.__init__(self, id, name)
+    def calculate_payroll(self):
+        return self.payroll.calculate_payroll()
--- inheritance_vs_composition/main.py
+++ inheritance_vs_composition/main.py
@@ -1,22 +1,40 @@
-import employees
-import hr
-import productivity
+# import employees
+# import hr
+# import productivity
+# import contacts
 
-manager = employees.Manager(1, "Mary Poppins", 3000)
-secretary = employees.Secretary(2, "John Smith", 1500)
-sales_guy = employees.SalesPerson(3, "Kevin Bacon", 1000, 250)
-factory_worker = employees.FactoryWorker(4, "Jane Doe", 40, 15)
-temporary_secretary = employees.TemporarySecretary(5, "Robin Williams", 40, 9)
-employees = [
-    manager,
-    secretary,
-    sales_guy,
-    factory_worker,
-    temporary_secretary,
-]
+# manager = employees.Manager(1, "Mary Poppins", 3000)
+# manager.address = contacts.Address("121 Admin Rd", "Concord", "NH", "03301")
+# secretary = employees.Secretary(2, "John Smith", 1500)
+# secretary.address = contacts.Address("67 Paperwork Ave.", "Manchester", "NH", "03101")
+# sales_guy = employees.SalesPerson(3, "Kevin Bacon", 1000, 250)
+# factory_worker = employees.FactoryWorker(4, "Jane Doe", 40, 15)
+# temporary_secretary = employees.TemporarySecretary(5, "Robin Williams", 40, 9)
+# employees = [
+#     manager,
+#     secretary,
+#     sales_guy,
+#     factory_worker,
+#     temporary_secretary,
+# ]
 
-productivity_system = productivity.ProductivitySystem()
+# productivity_system = productivity.ProductivitySystem()
+# productivity_system.track(employees, 40)
+
+# payroll_system = hr.PayrollSystem()
+# payroll_system.calculate_payroll(employees)
+
+
+from hr import HourlyPolicy, PayrollSystem
+from productivity import ProductivitySystem
+from employees import EmployeeDatabase
+
+productivity_system = ProductivitySystem()
+payroll_system = PayrollSystem()
+employee_database = EmployeeDatabase()
+
+employees = employee_database.employees
+manager = employees[0]
+manager.payroll = HourlyPolicy(55)
 productivity_system.track(employees, 40)
-
-payroll_system = hr.PayrollSystem()
 payroll_system.calculate_payroll(employees)
